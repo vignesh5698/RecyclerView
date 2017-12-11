00ba977f6acaf71974fc566e6d2151b48b60dcea
vignesh5698 <vigneshit13@gmail.com>
2017-12-11 12:44:26 +0530

    Recycler View

--- app/src/main/java/com/example/vignesh/test/RecyclerAdapter.java
+++ app/src/main/java/com/example/vignesh/test/RecyclerAdapter.java
@@ -1,31 +1,42 @@
 package com.example.vignesh.test;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Created by vignesh on 11/12/17.
  */
 
-public class RecyclerAdapter extends RecyclerView.Adapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder> {
 
+    private ArrayList<DataProvider> arrayList=new ArrayList<DataProvider>();
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public RecyclerAdapter(ArrayList<DataProvider> arrayList){
+        this.arrayList=arrayList;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout,parent,false);
+
+        RecyclerViewHolder recyclerViewHolder=new RecyclerViewHolder(view);
+        return recyclerViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return arrayList.size();
     }
 
     public static class RecyclerViewHolder extends RecyclerView.ViewHolder{
